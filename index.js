84ca1437aa7072b771cf9bda82410a6cb2237073
Daniel <dkilcullen25@gmail.com>
2021-11-15 01:17:43 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const input = document.getElementById('input')
+
+function clickAlert() { alert('i was clicked!') }
+
 function addingEventListener() {
+    input.addEventListener('click',clickAlert)
 }
